f05c1f68ccc54024bd693036275417dce29f0aef
Kresley <kresleylucas.r@gmail.com>
2025-10-10 00:04:09 -0300

    feat: update .gitignore, package-lock.json, and global.d.ts; remove unused .env file; import Bootstrap CSS in index.tsx

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './style.css'
 import CategoryCard from '../../components/CategoryCard/CategoryCard';
